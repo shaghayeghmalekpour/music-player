1326a4b687944d852e1934b18e7a0b3e54b3e1cb
Shaghayegh Malekpour <mk.shaqa@gmail.com>
2021-10-10 19:06:12 +0330

    finish styling

---
+++ index.js
@@ -0,0 +1,6 @@
+const musicContainer = document.querySelector('music-container')
+const playBtn = document.querySelector('#play')
+const prevBtn = document.querySelector('#prev')
+const nextBtn = document.querySelector('#next')
+const audio = document.querySelector('#audio')
+const progress = document.querySelector('.progress')
