96de9d16f663802be9af6d4be5b5d5521faff650
Shaghayegh Malekpour <mk.shaqa@gmail.com>
2021-10-12 17:43:08 +0330

    add progress music and prev next button some little detailes $

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const cover = document.querySelector('#cover')
 //song titles
 const songs = ['another-love' , 'love-nwantiti', 'me-pase'];
 
-let songIndex = 1;
+let songIndex = 0;
 
 loadSong(songs[songIndex]);
 
@@ -37,8 +37,38 @@ function pauseSong(){
 
     audio.pause();
 }
+function prevSong(){
+    songIndex--;
 
+    if(songIndex< 0){
+        songIndex = songs.length - 1;
+    }
+    loadSong(songs[songIndex])
+    playSong();
+}
 
+function nextSong(){
+    songIndex++;
+    if(songIndex> songs.length - 1){
+        songIndex = 0;
+    }
+    loadSong(songs[songIndex])
+    playSong();
+}
+
+function updateProgress(e){
+    const {duration , currentTime} = e.srcElement;
+    const progressPercent = (currentTime / duration) * 100;
+    progress.style.width = `${progressPercent}%`
+}
+
+function setProgress(e){
+    const width = this.clientWidth
+    const clickX = e.offsetX
+    const duration = audio.duration
+
+    audio.currentTime = (clickX / width) * duration;
+}
 
 //play or puase button
 playBtn.addEventListener('click', () => {
@@ -50,3 +80,13 @@ playBtn.addEventListener('click', () => {
         playSong()
     }
 })
+
+//change songs 
+prevBtn.addEventListener('click', prevSong);
+nextBtn.addEventListener('click', nextSong);
+
+audio.addEventListener('timeupdate', updateProgress)
+
+progressContainer.addEventListener('click', setProgress)
+
+audio.addEventListener('ended', nextSong)
