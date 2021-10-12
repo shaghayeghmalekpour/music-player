cbff0794916e217489c8be6a7baee988e62bce46
Shaghayegh Malekpour <mk.shaqa@gmail.com>
2021-10-12 16:16:16 +0330

    update songs detail

--- index.js
+++ index.js
@@ -4,3 +4,20 @@ const prevBtn = document.querySelector('#prev')
 const nextBtn = document.querySelector('#next')
 const audio = document.querySelector('#audio')
 const progress = document.querySelector('.progress')
+const progressContainer = document.querySelector('.progress-container')
+const title = document.querySelector('#title')
+const cover = document.querySelector('#cover')
+
+//song titles
+const songs = ['another-love' , 'love-nwantiti', 'me-pase'];
+
+let songIndex = 1;
+
+loadSong(songs[songIndex]);
+
+//update songs detailes
+function loadSong(song){
+    title.innerText = song;
+    audio.src = `./music/${song}.mp3` ;
+    cover.src = `./images/${song}.jpg` ;
+}
